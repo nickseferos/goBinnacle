b44697c318be8a6eccd2cf930c05ceaae34dc9a8
nickseferos <nick.seferos@gmail.com>
2016-01-02 23:06:45 -0800

    Fixed landing page issue.
    Posts wouldn't order corrently. Added order by date and retieved the last.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def index
-    @satire_last_post = Satire.where('date <= ?', Date.today).last
-    @editorial_last_post = Editorial.where('date <= ?', Date.today).last
-    @other_last_post = Other.where('date <= ?', Date.today).last
+    @satire_last_post = Satire.where('date <= ?', Date.today).order(:date).last
+    @editorial_last_post = Editorial.where('date <= ?', Date.today).order(:date).last
+    @other_last_post = Other.where('date <= ?', Date.today).order(:date).last
     @events = Event.where('date >= ?', Date.today).order(:date).paginate(:page => params[:page], :per_page => 3)
   end
 
